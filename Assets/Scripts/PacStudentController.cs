2e3f9adf323b5a3b382accd95a666c7105bea7eb
ClodaghMM <13559048@student.uts.edu.au>
2021-10-13 18:05:42 +1100

    Added studentController script

---
+++ Assets/Scripts/PacStudentController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PacStudentController : MonoBehaviour
+{
+
+    //Implement movement in the following way
+    //linear lerping to move from one grid position to another. //lerp at fixed speed and be frame-rate independent (time.fixedDeltaTime)
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
