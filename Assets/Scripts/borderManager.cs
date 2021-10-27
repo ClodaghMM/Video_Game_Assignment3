4f6c03d9000f543ae2fc381e12f8ec229e3e4d05
ClodaghMM <13559048@student.uts.edu.au>
2021-10-27 15:55:47 +1100

    Finished border

--- Assets/Scripts/borderManager.cs
+++ Assets/Scripts/borderManager.cs
@@ -9,12 +9,19 @@ public class borderManager : MonoBehaviour
     private int updateTime = 2;
     private Tweener tweener;
     public GameObject starPos1;
-    private Animator animator; 
+    private Animator animatorPos1; 
+    private Animator animatorPos2;
+    private Animator animatorPos3;
+    private Animator animatorPos4;
 
     // Start is called before the first frame update
     void Start()
     {
-        animator = starPos1.GetComponent<Animator>();
+        animatorPos1 = starsTransform[0].
+        GetComponent<Animator>();
+        animatorPos2 = starsTransform[1].GetComponent<Animator>();
+        animatorPos3 = starsTransform[2].GetComponent<Animator>();
+        animatorPos4 = starsTransform[3].GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -22,9 +29,18 @@ public class borderManager : MonoBehaviour
     {
         timer += Time.deltaTime;
         if((int)timer == updateTime){
-            animator.SetTrigger("moveNow");
+            animatorPos1.SetTrigger("moveNow");
+            animatorPos1.SetTrigger("moveBack");
+            animatorPos2.SetTrigger("moveDown");
+            animatorPos2.SetTrigger("downBack");
+            animatorPos3.SetTrigger("moveLeft");
+            animatorPos3.SetTrigger("LeftBack");
+            animatorPos4.SetTrigger("MoveUp");
+            animatorPos4.SetTrigger("upBack");
+
+        updateTime += 2;
         }
-            
+        
     
     }
 }
